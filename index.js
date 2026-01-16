dc1882147264dfb5595194dab3f350f7e3a29ec8
scandalouswraith <blondebob7@yahoo.com>
2026-01-16 10:43:42 -0600

    Fix syntax errors and reformat index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const levelRoles = [
   { level: 50, name: "🍻 Tavern Fixture" },
   { level: 65, name: "🏆 Keeper's Confidant" }
   { level: 80, name: "🏆 Legend of the Hearth" },
-  { level: 100, name: "🏆 Whiskered Legend" },
+  { level: 100, name: "🏆 Whiskered Legend" }
 ];
 
 // 🌟 Greetings & Goodbyes
@@ -190,6 +190,7 @@ client.on("messageCreate", message => {
 
 // 🔐 Login
 client.login(process.env.DISCORD_TOKEN);
+
 
 
 
